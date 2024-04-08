8a5de75dea299e466d6b01b10eb549a9cc8b1756
KateGomel <ykatuay@gmail.com>
2024-04-08 22:23:46 +0300

    add singIn & singUp

--- src/pages/sing-in/sing-in.component.js
+++ src/pages/sing-in/sing-in.component.js
@@ -1,10 +1,13 @@
 import { Component } from "../../core/Component";
 import template from "./sing-in.template.hbs";
+import { ROUTES } from "../../constants/routes";
 
 export class SingIn extends Component {
   constructor() {
     super();
-    this.template = template();
+    this.template = template({
+      routes: ROUTES,
+    });
   }
 }
 
--- src/pages/sing-up/sing-up.component.js
+++ src/pages/sing-up/sing-up.component.js
@@ -1,10 +1,13 @@
 import { Component } from "../../core/Component";
 import template from "./sing-up.template.hbs";
+import { ROUTES } from "../../constants/routes";
 
 export class SingUp extends Component {
   constructor() {
     super();
-    this.template = template();
+    this.template = template({
+      routes: ROUTES,
+    });
   }
 }
 
