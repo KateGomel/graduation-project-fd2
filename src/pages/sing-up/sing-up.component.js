6eb87b71bc0d0f3d64ad1d0699379401c814e954
KateGomel <ykatuay@gmail.com>
2024-04-13 13:21:35 +0300

    fix toast

--- src/pages/sing-up/sing-up.component.js
+++ src/pages/sing-up/sing-up.component.js
@@ -1,6 +1,9 @@
 import { Component } from "../../core/Component";
 import template from "./sing-up.template.hbs";
 import { ROUTES } from "../../constants/routes";
+import { eventEmitter } from "../../core/EventEmitter";
+import { EVENT_TYPES } from "../../constants/eventTypes";
+import { TOAST_TYPE } from "../../constants/toast";
 
 export class SingUp extends Component {
   constructor() {
@@ -11,6 +14,13 @@ export class SingUp extends Component {
     this.state = {
       isLoading: false,
     };
+  }
+
+  componentDidMount() {
+    eventEmitter.emit(EVENT_TYPES.toastNotification, {
+      type: TOAST_TYPE.info,
+      message: "Hi",
+    });
   }
 }
 
