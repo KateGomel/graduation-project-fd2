f8d3ef2b1a217277acbe5e2895c98faddc649b6e
KateGomel <ykatuay@gmail.com>
2024-04-14 12:56:29 +0300

    add authorize

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -3,13 +3,43 @@ import template from "./home.template.hbs";
 import { ROUTES } from "../../constants/routes";
 
 import "../../components/router-link/router-link.component";
+import { useUserStore } from "../../hooks/useUserStore";
 
 export class HomePage extends Component {
   constructor() {
     super();
-    this.template = template({
-      routes: ROUTES,
-    });
+    this.template = template({ routes: ROUTES });
+    this.state = {
+      links: [
+        {
+          label: "Sing In",
+          href: ROUTES.signIn,
+        },
+        {
+          label: "Sing Up",
+          href: ROUTES.signUp,
+        },
+      ],
+    };
+  }
+
+  setLinks = () => {
+    const { getUser } = useUserStore();
+    const user = getUser();
+    if (getUser()) {
+      this.setState({
+        links: [
+          {
+            label: "Dashboard",
+            href: ROUTES.dashboard,
+          },
+        ],
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.setLinks();
   }
 }
 
