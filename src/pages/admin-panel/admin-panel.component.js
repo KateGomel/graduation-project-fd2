dd9b619f84036a9e08a90966aa20b959d9cc5f74
KateGomel <ykatuay@gmail.com>
2024-04-14 21:05:59 +0300

    add template admin-panel

--- src/pages/admin-panel/admin-panel.component.js
+++ src/pages/admin-panel/admin-panel.component.js
@@ -1,6 +1,11 @@
 import { ROUTES } from "../../constants/routes";
 import { Component } from "../../core/Component";
 import template from "./admin-panel.template.hbs";
+import { useUserStore } from "../../hooks/useUserStore";
+import { authService } from "../../services/Auth";
+import { useToastNotification } from "../../hooks/useToastNotification";
+import { TOAST_TYPE } from "../../constants/toast";
+import { useNavigate } from "../../hooks/useNavigate";
 
 export class AdminPanel extends Component {
   constructor() {
@@ -10,7 +15,57 @@ export class AdminPanel extends Component {
     });
     this.state = {
       isLoading: false,
+      user: null,
+      board: [],
     };
+  }
+
+  toggleIsLoading = () => {
+    this.setState({
+      ...this.state,
+      isLoading: !this.state.isLoading,
+    });
+  };
+
+  logout = () => {
+    this.toggleIsLoading();
+    const { setUser } = useUserStore();
+    authService
+      .logOut()
+      .then(() => {
+        setUser(null);
+        useToastNotification({ type: TOAST_TYPE.success, message: "Success!" });
+        useNavigate(ROUTES.signIn);
+      })
+      .catch(({ message }) => {
+        useToastNotification({ message });
+      })
+      .finally(() => {
+        this.toggleIsLoading();
+      });
+  };
+
+  onClick = ({ target }) => {
+    if (target.closest(".logout-btn")) {
+      this.logout();
+    }
+  };
+
+  setUser() {
+    const { getUser } = useUserStore();
+    this.setState({
+      ...this.state,
+      user: getUser(),
+    });
+  }
+
+  componentDidMount() {
+    this.setUser();
+    this.addEventListener("click", this.onClick);
+  }
+
+  componentWillUnmount() {
+    this.removeEventListener("click", this.onClick);
   }
 }
 
--- src/pages/dashboard/dashboard.component.js
+++ src/pages/dashboard/dashboard.component.js
@@ -19,6 +19,7 @@ export class Dashboard extends Component {
       board: [],
     };
   }
+
   toggleIsLoading = () => {
     this.setState({
       ...this.state,
