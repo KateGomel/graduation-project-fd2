34cee984ea1d324c802b70d22fcb5973b3f1c9fc
KateGomel <ykatuay@gmail.com>
2024-04-17 21:39:42 +0300

    add works.js

---
+++ src/constants/words.js
@@ -0,0 +1,22 @@
+export const words = [
+  {
+    group: 1,
+    translate: "начинать",
+    infinitive: "begin",
+    infTranscription: "[bɪ`gɪn]",
+    pastSimple: "began",
+    simTranscription: "[bɪ`gæn]",
+    pastParticiple: "begun",
+    partTranscription: "[bɪ`gʌn]",
+  },
+  {
+    group: 1,
+    translate: "пить",
+    infinitive: "drink",
+    infTranscription: "[drɪnk]",
+    pastSimple: "drank",
+    simTranscription: "[drænk]",
+    pastParticiple: "drunk",
+    partTranscription: "[drʌnk]",
+  },
+];
