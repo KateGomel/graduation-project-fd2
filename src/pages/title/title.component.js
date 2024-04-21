0b4f3f853161c2244ef31ce06459c2efb0a80431
KateGomel <ykatuay@gmail.com>
2024-04-21 12:51:05 +0300

    dd

--- src/pages/title/title.component.js
+++ src/pages/title/title.component.js
@@ -53,9 +53,6 @@ export class Title extends Component {
     if (target.closest(".btn")) {
       this.createdArrayWords();
     }
-    if (target.closest(".toWord")) {
-      useNavigate(ROUTES.words);
-    }
   };
 
   setUser() {
